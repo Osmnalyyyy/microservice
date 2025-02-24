b8a4edbbc78e88a5f4cd3a9bc2e708c72cf40110
Osmnalyyyy <Sevinc32isparta@gmail.com>
2025-02-24 15:22:32 +0300

    add services

---
+++ ServiceC/src/main/java/com/ms/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.ms.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping
+public class HelloController {
+
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Service C Hello" + LocalDateTime.now();
+    }
+}
